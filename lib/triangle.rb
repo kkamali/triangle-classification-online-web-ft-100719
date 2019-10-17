be221c30063affd93c2b81e363fea58905da0d60
Kathleen Kamali <kathleenkamali@gmail.com>
2019-10-17 21:23:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,10 @@ class Triangle
   end
   
   def kind
-    if @side_1 == @side_2 && @side_1 == @side_3 
+    if @side_1 <= 0 || @side_2 <= 0 || @side_3 <=0 
+      raise TriangleError 
+      return nil 
+    elsif @side_1 == @side_2 && @side_1 == @side_3 
       return :equilateral
     elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
       return :isosceles
